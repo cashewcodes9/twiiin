a5aa7b63ae06951fd9bc1351d178045fce3f6cef
cashewcodes9 <cashewcodes9@gmail.com>
2022-07-31 23:49:34 +0200

    migration file modified for voucher!

--- database/migrations/2022_07_28_213520_create_vouchers_table.php
+++ database/migrations/2022_07_28_213520_create_vouchers_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('vouchers', function (Blueprint $table) {
            $table->id();
-           $table->integer('user_id')->nullable()->default(0);
            $table->string('voucher', 8)->unique()->default('');
            $table->integer('expiry')->nullable()->default(0);
+           $table->timestamp('expires_at');
            $table->timestamps();
         });
     }
