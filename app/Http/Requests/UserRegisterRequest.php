5af69a4ffcf7040b9f3346c96d4319bdf7338f84
cashewcodes9 <cashewcodes9@gmail.com>
2022-08-01 00:13:53 +0200

    Voucher validation added while registering new users!

--- app/Http/Requests/UserRegisterRequest.php
+++ app/Http/Requests/UserRegisterRequest.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Requests;
 
+use Exception;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
+use Illuminate\Validation\Rule;
 
 class UserRegisterRequest extends FormRequest
 {
@@ -24,9 +29,34 @@ class UserRegisterRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'=>'required|string|max:255',
+            'voucher' => [
+                'required',
+                'min:8',
+                'max:8',
+                'string',
+                Rule::exists('vouchers', 'voucher')->where(function (Builder $query) {
+                    return $query->where([
+                        ['voucher', '=', strtoupper($this->get('voucher'))],
+                        ['expires_at', '>', Carbon::now()],
+                    ]);
+                })
+            ],
             'email'=>'required|string|email|max:255|unique:users',
             'password'=>'required|string|min:8',
         ];
+    }
+
+    /**
+     * Custom error messages
+     * @return array
+     * @throws Exception
+     */
+    public function messages(): array
+    {
+        return Arr::dot([
+            'voucher' => [
+                'exists' => 'You can only used your voucher once. Please make sure you are using valid voucher!',
+            ]
+        ]);
     }
 }
